d521fd803f742461c1c4bb4f7b574d3439424149
ProtoBit <bobrow2006@gmail.com>
2020-09-16 19:54:12 +0100

    Added ImGUI and ImGUI SDL and nearly implemented init for it

--- VTileEdit/VTile Edit/main.cpp
+++ VTileEdit/VTile Edit/main.cpp
@@ -1,6 +1,70 @@
 ﻿#include "main.h"
 
+struct colour
+{
+	int r = 0;
+	int b = 0;
+	int g = 0;
+	int a = 0;
+};
+
+typedef std::vector<colour> palette_t;
+
+class bitplane
+{
+	std::vector<bool> bits;
+};
+
+
+class pixel
+{
+public:
+	int index;
+	std::vector<bitplane>* m_Bitplanes;
+	pixel(std::vector<bitplane>* bitplanes, int _index)
+	{
+		index = _index;
+		m_Bitplanes = bitplanes;
+	}
+
+};
+
+struct vec2
+{
+	int x, y;
+};
+
+class tile
+{
+public:
+	vec2 m_Size;
+	std::vector<bitplane> m_Bitplanes;
+	std::vector<pixel> pixels;
+	tile(vec2 size, int bpp)
+	{
+		m_Size = size;
+		m_Bitplanes.resize(bpp);
+		for (int x = 0; x < m_Size.x; x++)
+		{
+			for (int y = 0; y < m_Size.x; y++)
+			{
+				pixel temp(&m_Bitplanes, x * y);
+				pixels.push_back(temp);
+			}
+		}
+	}
+};
+
+typedef std::vector<tile> tileMap;
+
 int main(int argc, char* argv[])
 {
+	palette_t palette;
+	palette.resize(256);
+
+	SDL_Window* imgui_w = SDL_CreateWindow("Controls - VTileEdit", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_VULKAN);
+	SDL_Renderer* imgui_r = SDL_CreateRenderer(imgui_w, -1, SDL_RENDERER_ACCELERATED);
+	ImGuiSDL::Initialize(imgui_r, 800, 600);
+	
 	return 0;
 }
--- VTileEdit/VTile Edit/main.h
+++ VTileEdit/VTile Edit/main.h
@@ -1,5 +1,8 @@
 ﻿#pragma once
 
 #include <iostream>
-#include <SDL.h>
+#include "../libs/SDL2/include/SDL.h"
+#include <vector>
+#include "../libs/ImGUI/examples/imgui_impl_sdl.h"
+#include "../libs/ImGUI_SDL2/imgui_sdl.h"
 // TODO: Reference additional headers your program requires here.
